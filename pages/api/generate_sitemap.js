c92d06f52ec3141eff1e2b27a063263454daee2a
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-02-21 00:31:21 +0530

    Add priority in sitemap

--- pages/api/generate_sitemap.js
+++ pages/api/generate_sitemap.js
@@ -3,12 +3,14 @@ import process from "process";
 export const generateSitemap = async (posts) => {
   var BASE_URL = "https://www.ajaichemmanam.vercel.app";
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
   <url>
       <loc>${BASE_URL}</loc>
+      <priority>1.00</priority>
   </url>
   <url>
-    <loc>${BASE_URL}/blog</loc>
+    <loc>${BASE_URL}/blogs</loc>
+    <priority>0.80</priority>
   </url>
 ${[...posts]
   .map(
@@ -16,6 +18,7 @@ ${[...posts]
   )
   .join("")}  <url>
     <loc>${BASE_URL}/projects</loc>
+    <priority>0.64</priority>
   </url>
 </urlset>`.replace(",", "");
   // writes sitemap.xml to public directory
