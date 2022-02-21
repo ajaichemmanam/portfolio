b485b9cde32d847fc9cb52788c43d4bdab3ea42f
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-02-21 13:05:36 +0530

    Improve Sitemap

--- pages/api/generate_sitemap.js
+++ pages/api/generate_sitemap.js
@@ -5,22 +5,26 @@ export const generateSitemap = async (posts) => {
   var BASE_URL = "https://www.ajaichemmanam.vercel.app";
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-  <url>
-      <loc>${BASE_URL}</loc>
-      <priority>1.00</priority>
-  </url>
-  <url>
-    <loc>${BASE_URL}/blogs</loc>
-    <priority>0.80</priority>
-  </url>
+<url>
+  <loc>${BASE_URL}</loc>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>${BASE_URL}/blogs</loc>
+  <priority>0.80</priority>
+</url>
 ${[...posts]
   .map(
-    (post) => `<url><loc>${BASE_URL}/blogposts/${post.params.id}</loc></url>`
+    (post) => `<url>
+  <loc>${BASE_URL}/blogposts/${post.params.id}</loc>
+  <priority>0.80</priority>
+</url>`
   )
-  .join("")}  <url>
-    <loc>${BASE_URL}/projects</loc>
-    <priority>0.64</priority>
-  </url>
+  .join(`\n`)}  
+<url>
+  <loc>${BASE_URL}/projects</loc>
+  <priority>0.64</priority>
+</url>
 </urlset>`.replace(",", "");
   // writes sitemap.xml to public directory
   const path = `${process.cwd()}/public/sitemap.xml`;
