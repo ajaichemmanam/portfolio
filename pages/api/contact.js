f9a9d11aa4878d8ed762c5d3dbfed9d50e327db4
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-02-21 11:30:24 +0530

    Add logs to debug

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,6 +1,8 @@
 import { SMTPClient } from "emailjs";
 import process from "process";
 export default async function handler(req, res) {
+  console.log("contact_req", req.body);
+  console.log("process.env", process.env);
   if (req.method === "POST") {
     const client = new SMTPClient({
       user: process.env.FROM_EMAIL_USERNAME,
@@ -17,12 +19,14 @@ export default async function handler(req, res) {
         subject: "Contact Message from Portfolio",
       });
     } catch (e) {
+      console.log("Error sending mail in contact api", e);
       res.status(400).end(JSON.stringify({ message: "Error" }));
       return;
     }
-
+    console.log("response contact api: mail sent");
     res.status(200).end(JSON.stringify({ message: "Mail Sent" }));
   } else {
+    console.log("response contact api: method not allowed");
     res.status(400).end(JSON.stringify({ message: "Method Not Allowed" }));
   }
 }
