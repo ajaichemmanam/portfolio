9ef87bf3b539f64caf8ffe1f2aa7029572187ba1
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-03-04 08:52:36 +0530

    Fix dark mode issue on blogposts

--- pages/blogposts/[id].js
+++ pages/blogposts/[id].js
@@ -26,17 +26,17 @@ export default function BlogPost({ postData }) {
     <Layout>
       <div className="mx-auto mt-10 px-4 pb-28 sm:mt-16 sm:px-6 md:px-8 xl:px-12 xl:max-w-6xl">
         <article className="relative pt-10 max-w-3xl mx-auto xl:max-w-none xl:grid xl:grid-cols-[1fr_50rem] xl:gap-x-8">
-          <div className="hidden mb-5 pb-5 border-b border-slate-200 xl:block dark:border-slate-200/5">
+          <div className="hidden mb-5 pb-5 border-b border-slate-200/5 xl:block">
             <Link href="/blogs">
-              <a className="group flex font-semibold text-slate-700 hover:text-slate-900 dark:text-slate-200 dark:hover:text-white">
+              <a className="group flex font-semibold text-slate-200 hover:text-white">
                 &larr; Back to Blog List
               </a>
             </Link>
           </div>
-          <div className="col-span-full text-3xl sm:text-4xl sm:text-center xl:mb-16 font-extrabold tracking-tight text-slate-900 dark:text-slate-200 ">
+          <div className="col-span-full text-3xl sm:text-4xl sm:text-center xl:mb-16 font-extrabold tracking-tight text-slate-200">
             {postData.title}
           </div>
-          <div className="absolute top-0 inset-x-0 text-slate-700 sm:text-center dark:text-slate-400">
+          <div className="absolute top-0 inset-x-0 text-slate-400 sm:text-center">
             Date: {postData.date}
           </div>
           <div className="text-sm leading-6 mb-16 mx-auto xl:mb-0">
@@ -51,7 +51,7 @@ export default function BlogPost({ postData }) {
             />
             Written by {postData.author}
           </div>
-          <div className="prose prose-slate dark:prose-dark">
+          <div className="prose prose-dark">
             <MarkDownFormatter markdown={postData.content} />
           </div>
         </article>
