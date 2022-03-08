416b04043ca9a4413b959f7bba9d9157d9a21b4b
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-03-08 11:24:58 +0530

    Use a new method to render sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,79 @@
+import fs from "fs";
+import { getAllPostIds } from "../lib/posts";
+
+const Sitemap = () => {};
+
+export const getServerSideProps = async ({ res }) => {
+  const baseUrl = {
+    development: "http://localhost:3000",
+    production: "https://ajaichemmanam.vercel.app",
+  }[process.env.NODE_ENV];
+
+  const staticPages = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "api",
+        "blogposts",
+        "about.js",
+        "index.js",
+        "skills.js",
+        "testimonials.js",
+        "contact.js",
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${baseUrl}/${staticPagePath.split(".")[0]}`;
+    });
+
+  const posts = await getAllPostIds();
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+    <loc>${baseUrl}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+      ${posts
+        .map((post) => {
+          return `
+              <url>
+                <loc>${baseUrl}/blogposts/${post.params.id}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>1.0</priority>
+              </url>
+            `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+};
+
+export default Sitemap;
