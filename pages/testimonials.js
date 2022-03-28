87617e805f4cf13dc74b0ed573d6a9b4233fe301
Ajai John Chemmanam <ajaichemmanam@gmail.com>
2022-03-28 21:19:19 +0530

    improve testimonial styling

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -54,7 +54,7 @@ export default function Testimonials() {
         <div className="flex flex-wrap m-4">
           {testimonials.map((testimonial) => (
             <div className="p-4 md:w-1/2 w-full">
-              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded flex flex-col justify-between items-center">
                 <blockquote className="leading-relaxed relative mb-6 p-5 m-1">
                   {testimonial.quote}
                 </blockquote>
